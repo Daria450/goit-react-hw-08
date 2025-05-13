c7cea81cdcaf609f28641cae69b466550930d841
Daria <shirai.ryu.hanzo.hasashi@gmail.com>
2025-05-14 01:13:24 +0300

    homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom"
 
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hello there</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <Link to="/register" className="btn btn-accent">Sign up</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
